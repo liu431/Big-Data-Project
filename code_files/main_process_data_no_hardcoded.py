5044140b66c4d87057a36b1cac187b50609ed433
Sanittawan Tan <sanittawan.t@gmail.com>
2019-05-16 12:12:38 -0500

    Updated the code

--- code_files/main_process_data_no_hardcoded.py
+++ code_files/main_process_data_no_hardcoded.py
@@ -6,6 +6,21 @@ CreationDate, Score, ViewCount, Body, OwnerUserId, LastEditorUserId,
 LastEditorDisplayName, LastEditDate, LastActivityDate, 
 Title, Tags, AnswerCount, CommentCount, FavoriteCount, 
 CommunityOwnedDate, CommentCount]
+
+"<p>Hi, I'm not really a person.</p>
+
+<p>I'm a background process that helps keep this site clean!</p>
+
+<p>I do things like</p>
+
+<ul>
+<li>Randomly poke old unanswered questions every hour so they get some attention</li>
+<li>Own community questions and answers so nobody gets unnecessary reputation from them</li>
+<li>Own downvotes on spam/evil posts that get permanently deleted</li>
+<li>Own suggested edits from anonymous users</li>
+<li><a href=""http://meta.stackexchange.com/a/92006"">Remove abandoned questions</a></li>
+</ul>
+"
 '''
 
 import csv
@@ -25,13 +40,12 @@ def xml_to_csv(file_name, max_lines=0):
     parser.setContentHandler(Handler)
     parser.parse(file_name)
 
-'''
-Courtesy of Nikki
-'''
+
 def write_row_to_csv(row, out_filename):
     with open(out_filename, "a", newline='') as output:
         wr = csv.writer(output, dialect='excel')
         wr.writerow(row)
+
 
 class SOHandler(xml.sax.ContentHandler):
     def __init__(self, output_name, max_lines):
@@ -47,9 +61,9 @@ class SOHandler(xml.sax.ContentHandler):
         self.CurrentData = tag
         if tag == "row":
             if not self.limit_lines:
-                self.m_lines = self.m_lines + 1
+                self.m_lines += 1
             if self.row < self.m_lines:
-                self.row = self.row + 1
+                self.row += 1
                 if self.row == 1:
                     self.attrs = list(attributes.keys())
                     write_row_to_csv(self.attrs, self.out)
@@ -59,7 +73,8 @@ class SOHandler(xml.sax.ContentHandler):
                         if a == 'AboutMe':
                             if a in attributes:
                                 soup = BeautifulSoup(attributes[a], 'lxml')
-                                ###
+                                about_me_text = soup.get_text()
+                                row_to_write.append(about_me_text)
                         else:
                             val = attributes.get(a, '')
                             row_to_write.append(val)
@@ -70,3 +85,5 @@ if (__name__ == "__main__"):
         xml_to_csv(sys.argv[1])
     if len(sys.argv) == 3:
         xml_to_csv(sys.argv[1], int(sys.argv[2]))
+
+
