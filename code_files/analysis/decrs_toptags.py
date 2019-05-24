d7674df8898b8c70c7ddf05ecd6bb32bdc9b0b5a
dhruvalb <43822674+dhruvalb@users.noreply.github.com>
2019-05-24 16:47:02 -0500

    Top Tags Implemented with MRJob

---
+++ code_files/analysis/decrs_toptags.py
@@ -0,0 +1,45 @@
+'''
+Using MapReduce to find the top count for tags
+'''
+
+import heapq
+from mrjob.job import MRJob
+
+TOP_K = 15
+
+class MRTopTags(MRJob):
+
+    '''
+    MRJob to find the top 10 staff visited.
+    key - name - lastname, firstname
+    value - number of visits
+    '''
+
+    def mapper(self, _, line):
+        row = line.split(',')
+        if row[2] != 'Count':    
+            tag_name = row[1]
+            count = int(row[2])
+
+            yield (tag_name, count), None
+
+    def reducer_init(self):
+        self.h = []
+
+    def reducer(self, key, _):
+        tag = key[0]
+        count = key[1]
+
+        if len(self.h) < TOP_K:
+            heapq.heappush(self.h, (count, tag))
+        else:
+            if (count, tag) > self.h[0]:
+                spillover = heapq.heapreplace(self.h, (count, tag))
+
+    def reducer_final(self):
+        self.h.sort(reverse=True)
+        for entry in self.h:
+            yield entry
+
+if __name__ == '__main__':
+    MRTopTags.run()
