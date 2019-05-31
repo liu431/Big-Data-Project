f6c1584359793f497220d3fcd9388a7fb5a401ca
LI LIU <lliu95877@gmail.com>
2019-05-30 22:49:03 -0500

    Update

--- code_files/analysis/SentimentPopularity/Python/sentiment.py
+++ code_files/analysis/SentimentPopularity/Python/sentiment.py
@@ -7,16 +7,12 @@ Created on Sun May 26 12:53:12 2019
 
 from mrjob.job import MRJob
 import csv
-import datetime
 import numpy as np
-
-#from datetime import date
-
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-analyser = SentimentIntensityAnalyzer()
 
+#read in the selected indices
 anskey={}
-with open("out.txt") as f:
+with open("index.txt") as f:
     for line in f:
         pair = line.split("\t")      
         anskey[pair[0][1:-1]] = int(pair[1][2:-3])
@@ -27,7 +23,10 @@ def sentiment_analyzer_scores(sentence):
     function takes in a string input
     returns the normalized sentiment score
     '''
+    
+    analyser = SentimentIntensityAnalyzer()
     score = analyser.polarity_scores(sentence)
+    
     return score["compound"]
 
 class MRScore(MRJob):
@@ -36,16 +35,18 @@ class MRScore(MRJob):
   '''
   
   def mapper(self, _, line):
-
+      '''
+      key: day (could be modfied to month or year)
+      value: tuple of sentiment score and viewcount of the question
+      '''
+      
       line = csv.reader([line]).__next__()
       try:
           if line[1] =='2':
               ID = line[0] 
               body = line[7]
-              #tt = line[4].split("-")
               tt = line[4]
               try:
-                  #date = datetime.date(int(tt[0]),int(tt[1]),int(tt[2].split("T")[0]))
                   day = tt[:10]
                   if ID in anskey :
                       senti = sentiment_analyzer_scores(body)
@@ -60,13 +61,21 @@ class MRScore(MRJob):
 
       
   def reducer(self, day, scores):
+      '''
+      key: day
+      value: [0,(1,2)]
+            0: average of the sentimenviewcount
+            1: average of the viewcount
+            2: counts of elements assciated with the key
+      '''
       
       res = np.array(list(scores))
       listlen = len(res)
       res.reshape(listlen, 2)
       resm = np.mean(res, axis=0)
     
-      yield day, (resm[0], resm[1])
+      yield day, [resm[0], (resm[1], listlen)]
+      #yield day, listlen
 
 if __name__ == '__main__':
   MRScore.run()
