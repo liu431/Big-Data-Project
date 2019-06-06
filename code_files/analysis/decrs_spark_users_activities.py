fbb36a55436ba9553df6e81a7d308dd9d8693041
Sanittawan Tan <sanittawan.t@gmail.com>
2019-06-06 15:29:10 -0500

    Added Spark implementation

--- code_files/analysis/decrs_spark_users_activities.py
+++ code_files/analysis/decrs_spark_users_activities.py
@@ -3,6 +3,24 @@ CMSC 12300 / CAPP 30123
 Project: Descriptive analysis Task 2
 
 '''
-from pyspark import SparkContext
+from pyspark.sql import SparkSession
+import sys
 
-sc = SparkContext()
+if len(sys.argv) != 3:
+  raise Exception("Exactly 2 arguments are required: <inputUri> <outputUri>")
+
+inputUri=sys.argv[1]
+outputUri=sys.argv[2]
+
+# index = 8 = owneruserID
+
+#sc = pyspark.SparkContext()
+spark = SparkSession.builder.appName('users').getOrCreate()
+df = spark.read.csv(sys.argv[1], header="true")
+#distinct_users = df.select("OwnerUserId").distinct().count()
+#print(distinct_users)
+
+users = df.select("OwnerUserId").rdd.map(lambda x: (x, 1))
+users_counts = users.reduceByKey(lambda count1, count2: count1 + count2)    
+users_counts.saveAsTextFile(sys.argv[2])
+#distinct_users.saveAsTextFile(sys.argv[2])
