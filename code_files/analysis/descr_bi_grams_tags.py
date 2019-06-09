97c153a2e38013d72e535719a80fb530b0f546a5
Sanittawan Tan <sanittawan.t@gmail.com>
2019-06-08 19:00:06 -0500

    Nikki Added docstrings

--- code_files/analysis/descr_bi_grams_tags.py
+++ code_files/analysis/descr_bi_grams_tags.py
@@ -1,21 +1,28 @@
-'''
+"""
 CMSC 12300 / CAPP 30123
-Project: Descriptive analysis Task 2
+Task: Descriptive analysis (Exploring Tag Network)
 
-'''
-import string
-import re
+Main author: Sanittawan (Nikki)
+"""
 import csv
+import re
 from mrjob.job import MRJob
 
 class GetbiGramsTags(MRJob):
-    '''
-    docsring here
-    '''
+    """
+    Class for generating tag bi-grams using
+    MapReduce framework
+    """
     def mapper(self, _, line):
-        '''
-        docstring here
-        '''
+        """
+        Maps generated bi-grams with count
+
+        Inputs:
+            line: a single line in a CSV file
+
+        Returns: a sorted bi-grams as key
+            and count as value
+        """
         row = csv.reader([line]).__next__()
 
         try:
@@ -32,18 +39,33 @@ class GetbiGramsTags(MRJob):
 
 
     def combiner(self, tag, counts):
-        '''
-        docstring here
-        '''
+        """
+        Combine counts of all unique bi-grams
+
+        Inputs:
+            tag: (tuple) of tag bi-gram
+            counts: (int) of count
+
+        Returns: a sorted bi-grams as key
+            and intermediate sum of counts
+        """
         try:
             yield tag, sum(counts)
         except (TypeError, ValueError):
             pass
 
+
     def reducer(self, tag, counts):
-        '''
-        docstring here
-        '''
+        """
+        Reduce all counts of a unique bi-gram tag
+
+        Inputs:
+            tag: (tuple) of tag bi-gram
+            counts: (int) of count
+
+        Returns: a sorted bi-grams as key
+            and count as value
+        """
         try:
             yield tag, sum(counts)
         except (TypeError, ValueError):
