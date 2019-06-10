9bdb9e3957b076a3851e329ec98698ec28ba3e76
dhruvalb <43822674+dhruvalb@users.noreply.github.com>
2019-06-10 01:01:50 -0500

    Updated docstring for toptags

--- code_files/analysis/descr_toptags.py
+++ code_files/analysis/descr_toptags.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
 '''
 Using MapReduce to find the top count for tags
+
+Main Author: Dhruval Bhatt
 '''
 
 import heapq
@@ -9,14 +10,16 @@ from mrjob.job import MRJob
 TOP_K = 15
 
 class MRTopTags(MRJob):
-
-    '''
-    MRJob to find the top 10 staff visited.
-    key - name - lastname, firstname
-    value - number of visits
-    '''
-
+    """
+    MRJob to find the top 15 tags with highest count
+    """
     def mapper(self, _, line):
+        """
+        Maps tag name with total count
+        Inputs:
+            line: a single line in a CSV file
+        Returns: tag name and count as keys and no values
+        """
         row = line.split(',')
         if row[2] != 'Count':    
             tag_name = row[1]
@@ -25,9 +28,19 @@ class MRTopTags(MRJob):
             yield (tag_name, count), None
 
     def reducer_init(self):
+        """
+        Initializes Heap Structure
+        """
         self.h = []
 
     def reducer(self, key, _):
+        """
+        Populates top k tag names based on count
+        Inputs:
+            key: (tuple) tag name, count
+            _: None
+        Returns: None
+        """
         tag = key[0]
         count = key[1]
 
@@ -38,10 +51,14 @@ class MRTopTags(MRJob):
                 spillover = heapq.heapreplace(self.h, (count, tag))
 
     def reducer_final(self):
+        """
+        Yields top k sorted tag name and count
+        Inputs: None
+        Returns: count, tag name
+        """
         self.h.sort(reverse=True)
         for entry in self.h:
             yield entry
 
 if __name__ == '__main__':
     MRTopTags.run()
-
